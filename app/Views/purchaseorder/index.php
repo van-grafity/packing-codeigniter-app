d03b506a574851334a01fe9958da7aa8708518dd
ivansuhendra <ivan.suhendraaja@gmail.com>
2023-04-28 15:17:36 +0700

    split the buyer several sizes

--- app/Views/purchaseorder/index.php
+++ app/Views/purchaseorder/index.php
@@ -12,7 +12,6 @@
                 <li class="breadcrumb-item"><a href="<?= base_url('index.php/home'); ?>">Dashboard</a></li>
                 <li class="breadcrumb-item active"><?= $title; ?></li>
             </ol>
-            <!-- button Create -->
             <div class="row">
                 <div class="col">
                     <div class="card">
@@ -59,10 +58,9 @@
     </section>
 </div>
 
-<!-- Modal Create -->
 <form action="<?= base_url('index.php/purchaseorder/store'); ?>" method="post">
     <div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="ModalLabel">Add New Purchase Order</h5>
@@ -88,7 +86,7 @@
                     
                     <div class="form-group">
                         <label for="id">Factory</label>
-                        <select class="form-control" id="id" name="id">
+                        <select class="form-control" id="factory_id" name="factory_id">
                             <option value="">-- Select Factory --</option>
                             <?php foreach ($factory as $f) : ?>
                                 <option value="<?= $f['id']; ?>"><?= $f['name']; ?></option>
@@ -106,6 +104,7 @@
                         <input type="text" class="form-control" id="unit_price" name="unit_price" placeholder="Unit Price">
                     </div>
 
+                    
                     <div class="form-group">
                         <label for="PO_qty">PO Qty</label>
                         <input type="text" class="form-control" id="PO_qty" name="PO_qty" placeholder="PO Qty">
@@ -113,8 +112,72 @@
 
                     <div class="form-group">
                         <label for="PO_amount">PO Amount</label>
-                        <input type="text" class="form-control" id="PO_amount" name="PO_amount" placeholder="PO Amount">
+                        <input type="text" class="form-control" id="PO_amount" name="PO_amount" placeholder="0" readonly>
                     </div>
+
+                    <div class="form-group">
+                    
+                        <table class="table table-bordered" id="item_table">
+                            <thead>
+                                <tr>
+                                    <th>Size</th>
+                                    <th>Qty</th>
+                                    <th colspan="2" width="15%">Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <select class="form-control" id="size_id[]" name="size_id[]">
+                                            <option value="">-- Select Size --</option>
+                                            <?php foreach ($purchaseordersize as $s) : ?>
+                                                <option value="<?= $s['id']; ?>"><?= $s['size']; ?></option>
+                                            <?php endforeach; ?>
+                                    </td>
+                                    <td><input type="text" class="form-control" id="qty[]" name="qty[]" placeholder="Qty"></td>
+                                    <td style="text-align: center;">
+                                        <button type="button" class="btn btn-danger btn-sm"><i class="fas fa-minus" href="javascript:void(0);" onclick="removeRow(this);" id="btnRemoveRow"></i></button>
+                                    </td>
+                                    <td style="text-align: center;">
+                                        <button type="button" class="btn btn-success btn-sm"><i class="fas fa-plus" href="javascript:void(0);" onclick="addRow();" id="btnAddRow"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <table class="table table-bordered" id="style_table">
+                            <thead>
+                                <tr>
+                                    <th>Style No.</th>
+                                    <th>Style Description</th>
+                                    <th colspan="2" width="15%">Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <select class="form-control" id="style_no[]" name="style_no[]">
+                                            <option value="">-- Select Style No. --</option>
+                                            <?php foreach ($purchaseorderstyle as $s) : ?>
+                                                <option value="<?= $s['id']; ?>"><?= $s['style_no']; ?></option>
+                                            <?php endforeach; ?>
+                                    </td>
+                                    <td><input type="text" class="form-control" id="style_description[]" name="style_description[]" placeholder="Style Description"></td>
+                                    <td style="text-align: center;">
+                                        <button type="button" class="btn btn-danger btn-sm"><i class="fas fa-minus" href="javascript:void(0);" onclick="removeRow(this);" id="btnRemoveRow"></i></button>
+                                    </td>
+                                    <td style="text-align: center;">
+                                        <button type="button" class="btn btn-success btn-sm"><i class="fas fa-plus" href="javascript:void(0);" onclick="addRow();" id="btnAddRow"></i></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                   
+
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Add Purchase Order</button>
@@ -124,4 +187,87 @@
     </div>
 </form>
 
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script>
+    function addRow() {
+        var html = '<tr>';
+        html += '<td><select class="form-control" id="size_id[]" name="size_id[]"><option value="">-- Select Size --</option><?php foreach ($purchaseordersize as $s) : ?><option value="<?= $s['id']; ?>"><?= $s['size']; ?></option><?php endforeach; ?></select></td>';
+        html += '<td><input type="text" class="form-control" id="qty[]" name="qty[]" placeholder="Qty"></td>';
+        html += '<td><button type="button" class="btn btn-danger btn-sm"><i class="fas fa-minus" href="javascript:void(0);" onclick="removeRow(this);"></i></button></td>';
+        html += '<th><button type="button" class="btn btn-success btn-sm" onclick="addRow();"><i class="fas fa-plus"></i></button></th>';
+        html += '</tr>';
+        $('#item_table tbody').append(html);
+    }
+
+    function removeRow(e) {
+        try {
+            var row = $(e).closest('tr');
+            row.remove();
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    // function addRow() {
+    //     try {
+    //         var table = document.getElementById("item_table");
+    //         var rowCount = table.rows.length;
+    //         var row = table.insertRow(rowCount);
+
+    //         var cell1 = row.insertCell(0);
+    //         cell1.innerHTML = "<select class='form-control' id='size' name='size'><option value=''>-- Select Size --</option><?php foreach ($purchaseordersize as $s) : ?><option value='<?= $s['id']; ?>'><?= $s['size']; ?></option><?php endforeach; ?></select>";
+
+    //         var cell2 = row.insertCell(1);
+    //         cell2.innerHTML = "<input type='text' class='form-control' id='qty' name='qty' placeholder='Qty'>";
+
+    //         var cell3 = row.insertCell(2);
+    //         cell3.innerHTML = "<button type='button' class='btn btn-success btn-sm' onclick='addRow();'><i class='fas fa-plus'></i></button>";
+
+    //         var cell4 = row.insertCell(3);
+    //         cell4.innerHTML = "<button type='button' class='btn btn-danger btn-sm'><i class='fas fa-minus' href='javascript:void(0);' onclick='removeRow(this);'></i></button>";
+    //     } catch (e) {
+    //         alert(e);
+    //     }
+    // }
+
+    // function removeRow(oButton) {
+    //     try {
+    //         var table = document.getElementById("item_table");
+    //         table.deleteRow(oButton.parentNode.parentNode.rowIndex);
+    //     } catch (e) {
+    //         alert(e);
+    //     }
+    // }
+
+    $(document).ready(function() {
+        $('#unit_price, #PO_qty').keyup(function() {
+            var unit_price = $('#unit_price').val();
+            var PO_qty = $('#PO_qty').val();
+            var result = parseFloat(unit_price) * parseFloat(PO_qty);
+            if (!isNaN(result)) {
+                $('#PO_amount').val(result);
+            }
+        });
+    });
+
+    function calculate() {
+        var myBox1 = document.getElementById('unit_price').value;
+        var myBox2 = document.getElementById('PO_qty').value;
+        var result = document.getElementById('PO_amount');
+        var myResult = myBox1 * myBox2;
+        result.value = myResult;
+    }
+
+    $(document).ready(function() {
+        $('#unit_price').change(function() {
+            calculate();
+        });
+        $('#PO_qty').change(function() {
+            calculate();
+        });
+    });
+    
+</script>
 <?= $this->endSection(); ?>
