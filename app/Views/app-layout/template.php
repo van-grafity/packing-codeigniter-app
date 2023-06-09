df8916754c7d3dc43c1e75f64077fa6c6b5b7031
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-06-09 15:32:53 +0700

    add preloader

--- app/Views/app-layout/template.php
+++ app/Views/app-layout/template.php
@@ -227,6 +227,11 @@
             <!-- /.sidebar -->
         </aside>
 
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="<?= base_url('assets'); ?>/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+        </div>
+
         <?= $this->renderSection('content'); ?>
 
         <footer class="main-footer">
@@ -316,6 +321,7 @@
     <?= $this->renderSection('page_script'); ?>
 
 
+    // ## Script for set Active Class depand on Active Page
     <script>
         $(document).ready(function() {
             /*** add active class and stay opened when selected ***/
