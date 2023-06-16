47388b0468d60a34384180d13b651ff07223156b
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-06-16 17:36:10 +0700

    change new methode for dynamic base_url. bcs php spark cant run

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = BASE;
+    public string $baseURL = 'http://localhost/scan-pack/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
