cf60cd831350f021c14f085ad9259861fb7107ca
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-08-09 11:28:47 +0700

    change landing page bakcground

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,7 +30,10 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Welcome::index');
+// $routes->get('/', 'Welcome::index');
+$routes->get('/', function(){
+    return view('landing-page');
+});
 
 $routes->get('home', 'Home::index');
 $routes->get('login', 'Login::index');
