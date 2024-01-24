b77169a2381c484c4561573040305d91e0362f61
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2024-01-24 10:55:29 +0700

    add logo to replace default adminlte logo

--- app/Views/app-layout/template.php
+++ app/Views/app-layout/template.php
@@ -60,7 +60,7 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="<?= base_url('home') ?>" class="brand-link">
-                <img src="img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="img/app_logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light"><?= config('app')->applicationName; ?></span>
             </a>
             <!-- Sidebar -->
