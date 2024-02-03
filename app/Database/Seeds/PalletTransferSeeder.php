2d7026b6daa0d00e86d83f191663eb4908072f80
ridwanpranata <ridwanpranata10@gmail.com>
2024-02-03 22:59:33 +0700

    adjustment seeder

--- app/Database/Seeds/PalletTransferSeeder.php
+++ app/Database/Seeds/PalletTransferSeeder.php
@@ -13,23 +13,25 @@ class PalletTransferSeeder extends Seeder
         $data = [
             [
                 'id' => 1,
+                'transaction_number' => 'PTRF-2401-001',
                 'pallet_id' => 1,
                 'location_from_id' => 1,
                 'location_to_id' => 3,
                 'flag_transferred' => 'N',
                 'flag_loaded' => 'N',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('2024-01-01 H:i:s'),
+                'updated_at' => date('2024-01-01 H:i:s'),
             ],
             [
                 'id' => 2,
+                'transaction_number' => 'PTRF-2401-002',
                 'pallet_id' => 1751,
                 'location_from_id' => 2,
                 'location_to_id' => 3,
                 'flag_transferred' => 'N',
                 'flag_loaded' => 'N',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => date('2024-01-01 H:i:s'),
+                'updated_at' => date('2024-01-01 H:i:s'),
             ],
         ];
         $this->db->table($this->table)->insertBatch($data);
